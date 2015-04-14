034897a074bdc7cb592934863d35e308029151f2
Alexandr <justdealman@gmail.com>
2015-04-14 14:40:06 +0700

    original

---
+++ js/dynamic.js
@@ -0,0 +1,22 @@
+﻿function startPage() {
+	var h = $('.introduction').height();
+	var zoom = 1;
+	if ( h < 822 ) {
+		zoom = h/822;
+		h = 822;
+	}
+	$('.introduction > div').css({
+		'zoom': zoom,
+		'height': h+'px'
+	});
+}
+$(document).ready(function() {
+	if ( $('.introduction').length > 0 ) {
+		startPage();
+	}
+});
+$(window).resize(function() {
+	if ( $('.introduction').length > 0 ) {
+		startPage();
+	}
+});
